9e8de00fa4dd909e5083f830e6df3135c607b99d
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-13 18:40:02 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import CEP from './components/CEP';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.texto}> Digite seu CEP </Text>
+        
+        <TextInput
+          style={styles.input}
+          placeholder='11730-000'
+          keyboardType='numeric'
+        />
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}> Buscar </Text>
+        </TouchableOpacity>
+
+        <CEP/>
+      </View>
     </View>
   );
 }
@@ -17,4 +30,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    fontSize: 30,
+    alignSelf: 'center'
+  },
+  botao:{
+    width: '50%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    fontSize: 30,
+    borderRadius: 6,
+    backgroundColor: '#000',
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  textoBotao:{
+    color: '#fff',
+    fontSize: 30
+  },
+  input:{
+    width: '80%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    fontSize: 30,
+    borderRadius: 6,
+    backgroundColor: '#000',
+    color: '#fff',
+    marginTop: 10,
+    marginBottom: 10
+  }
 });
